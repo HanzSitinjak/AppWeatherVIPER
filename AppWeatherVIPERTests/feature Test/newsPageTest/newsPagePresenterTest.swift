65607d91b0b4435c92093f0975a225802f25ce81
Hanz Agung <hansagung06@gmail.com>
2024-11-06 15:20:33 +0700

    Update Test Unit/ Test Case newsPagePresenterTest

---
+++ AppWeatherVIPERTests/feature Test/newsPageTest/newsPagePresenterTest.swift
@@ -0,0 +1,17 @@
+//
+//  newsPagePresenterTest.swift
+//  AppWeatherVIPERTests
+//
+//  Created by Aleph-AHV2D on 06/11/24.
+//
+
+import XCTest
+import Nimble
+import Quick
+@testable import AppWeatherVIPER
+
+final class newsPagePresenterTest: QuickSpec {
+    override class func spec() {
+        <#code#>
+    }
+}
